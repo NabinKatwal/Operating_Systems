0547cc4017f6e8a9a42a4ba8c23e2bb8df1ce1af
Nabin Katwal <nabeenkatwal7@gmail.com>
2021-03-24 20:53:00 +0545

    Updated Shortest job first disk scheduling

---
+++ SJF.py
@@ -0,0 +1,37 @@
+from six.moves import input
+
+t = []
+n = int(input("Enter the number of process->"))
+processes = []
+for i in range(0,n):
+    processes.insert(i,i+1)
+print("Enter the burst time of the processes: \n")
+bt = list(map(int, input().split()))
+for i in range(0, len(bt)-1):
+    for j in range(0, len(bt)-i-1):
+        if(bt[j]>bt[j+1]):
+            temp = bt[j]
+            bt[j]=bt[j+1]
+            bt[j+1] = temp 
+            temp = processes[j]
+            processes[j] = processes[j+1]
+            processes[j+1] = temp
+wt = []
+avgwt = 0
+tat = []
+avgtat = 0
+wt.insert(0,0)
+tat.insert(0,bt[0])
+for i in range(1, len(bt)):
+    wt.insert(i, wt[i-1]+bt[i-1])
+    tat.insert(i, wt[i]+bt[i])
+    avgwt+=wt[i]
+    avgtat += tat[i]
+avgwt = float(avgwt)/n
+avgtat = float(avgtat)/n
+print('\n')
+print("Process \t Burst Time\t Waiting Time\t Turnaround Time")
+for i in range(0,n):
+    print(f"{str(processes[i])} \t\t {str(bt[i])}\t\t{str(wt[i])}\t\t{str(tat[i])}")
+print("Average waiting time is: "+str(avgwt))
+print("Average Turn around time is: "+str(avgtat))
